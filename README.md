35bc56216b80057266dc78750a8225c5c5d74711
bingoex <haibinwumr@163.com>
2017-04-09 23:32:47 +0800

    add introduct for replay

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # replay
-It is a packet replay tools
+It is a packet replay tools <br>
+一个发包压测工具
+- 单进程
+- 支持seq修改 //需用户定制
+- 支持简单压测报告(耗时\发包数\失败数)
 
 ## how to use it 
-
+```shell
 ./replay -h 
+```
--- replay.go
+++ replay.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"net"
-	"flag"
-	"log"
-	"io/ioutil"
-	"time"
-	"sync/atomic"
 	"encoding/binary"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
 	"os"
 	"os/signal"
+	"sync/atomic"
+	"time"
 )
 
 var (
-	pkgNum int
+	pkgNum          int
 	pkgNumPerPeriod int
-	remoteAddr string
-	pkgFile string
-	pkgToSend []byte
-	seqOff int
-	seq uint32
-	period int
+	remoteAddr      string
+	pkgFile         string
+	pkgToSend       []byte
+	seqOff          int
+	seq             uint32
+	period          int
 )
 
 func init() {
-	flag.IntVar(&pkgNum, "c", 10, "pkg count to send")//发包总数
-	flag.StringVar(&remoteAddr, "r", "183.60.48.140:8000", "remote addr to send pkg to")//发包目的地址
-	flag.StringVar(&pkgFile, "f", "req.bin", "pkg file to send")//发包内容，建议使用抓包工具直接将包抓下来
-	flag.IntVar(&pkgNumPerPeriod, "p", 10000, "pkg cnt per period")//每个周期发多少个包
-	flag.IntVar(&seqOff, "o", 2000, "seq offset")// TODO seq
-	flag.IntVar(&period, "t", 100, "period, in ms")//周期间隔时间
+	flag.IntVar(&pkgNum, "c", 10, "pkg count to send")                                   //发包总数
+	flag.StringVar(&remoteAddr, "r", "183.60.48.140:8000", "remote addr to send pkg to") //发包目的地址
+	flag.StringVar(&pkgFile, "f", "req.bin", "pkg file to send")                         //发包内容，建议使用抓包工具直接将包抓下来
+	flag.IntVar(&pkgNumPerPeriod, "p", 10000, "pkg cnt per period")                      //每个周期发多少个包
+	flag.IntVar(&seqOff, "o", 2000, "seq offset")                                        // TODO seq
+	flag.IntVar(&period, "t", 100, "period, in ms")                                      //周期间隔时间
 	flag.Parse()
 
 	var err error
@@ -42,29 +42,29 @@ func init() {
 }
 
 func nextSeq() uint16 {
-    var newSeq uint32
-    if seq >= (1<<16 - 1) {
-        newSeq = 0
-        atomic.StoreUint32(&seq, 0)
-    } else {
-        newSeq = atomic.AddUint32(&seq, 1)//原子操作
-    }
+	var newSeq uint32
+	if seq >= (1<<16 - 1) {
+		newSeq = 0
+		atomic.StoreUint32(&seq, 0)
+	} else {
+		newSeq = atomic.AddUint32(&seq, 1) //原子操作
+	}
 
-    return uint16(newSeq)
+	return uint16(newSeq)
 }
 
 func changeSeq(buf []byte) {
-    if len(buf) < 12 {
-        panic("buf is too small")
-    }
+	if len(buf) < 12 {
+		panic("buf is too small")
+	}
 
-    var bk [4]byte
-    tmp := bk[0:4]
+	var bk [4]byte
+	tmp := bk[0:4]
 
-    binary.BigEndian.PutUint16(tmp, nextSeq())//大端 ＝＝ 网络序
-    for i := 0; i < 2; i++ {
-        buf[1+2+2+i] = tmp[i]
-    }
+	binary.BigEndian.PutUint16(tmp, nextSeq()) //大端 ＝＝ 网络序
+	for i := 0; i < 2; i++ {
+		buf[1+2+2+i] = tmp[i]
+	}
 }
 
 func main() {
@@ -84,7 +84,7 @@ func main() {
 	sendCnt := 0
 	allCnt := 0
 
-	sendPkg := func () {
+	sendPkg := func() {
 		//changeSeq(pkgToSend) // 如果你需要改变序列号则重写该函数
 		conn.Write(pkgToSend)
 		sendCnt += 1
@@ -93,8 +93,8 @@ func main() {
 
 	var failSec uint64
 
-	begin :=time.Now()
-	start:= time.Now()
+	begin := time.Now()
+	start := time.Now()
 	for allCnt < pkgNum {
 		elapsed := time.Since(start) / (time.Millisecond * time.Duration(period))
 		if 0 == elapsed {
@@ -105,13 +105,14 @@ func main() {
 			if sendCnt < pkgNumPerPeriod {
 				failSec += 1
 			}
+
 			sendCnt = 0
 			start = time.Now()
 			sendPkg()
 		}
 	}
 
-	report := func () {
+	report := func() {
 		fmt.Printf("%d pkg, takes %v\n", pkgNum, time.Since(begin))
 		fmt.Printf("fail sec: %d\n", failSec)
 	}
